914d473552eaa3a3abb8e2b0cff9fd416666ae4a
wangzilaogong <997972547@qq.com>
2017-06-16 15:00:23 +0800

    h5

--- canvas.js
+++ canvas.js
@@ -1,4 +1,13 @@
 /**
  * Created by wk on 2017-06-15.
  */
-var canvas  =getElementById()
+ function draw(id){
+    var canvas = document.getElementById(id);
+    var  context =canvas.getContext('2d');
+    context.fillStyle = "#000";
+    context.strokeStyle = "#f60";
+    context.lineWidth = 5;
+    context.fillRect(0,0,400,300);
+    context.strokeRect(50,50,180,120);
+    context.strokeRect(100,100,180,120);
+}
